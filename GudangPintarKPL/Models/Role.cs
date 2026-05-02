d4a4a5bf1af47ad3476350761a525971d7e4f4cb
banufasaadigana-hash <banufasaadigana@gmail.com>
2026-05-02 10:36:40 +0700

    menambahkan dan memastikan lagi File Role dan User muncul

---
+++ GudangPintarKPL/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GudangPintarKPL.Models
+{
+    public enum Role
+    {
+        Admin,
+        User,
+        Karyawan,
+    }
+}
