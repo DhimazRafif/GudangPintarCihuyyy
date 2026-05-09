9fc56836b836c6c0d7ac1250b007d32bf8479001
SushiDotRoll <irsyad.spramudito@gmail.com>
2026-05-09 21:44:49 +0700

    Menambahkan DBC & Error Handling menggukanan assertion dan if else

--- GudangPintarKPL/GudangPintarKPL/Controllers/StockService.cs
+++ GudangPintarKPL/GudangPintarKPL/Controllers/StockService.cs
@@ -1,5 +1,6 @@
 ﻿using GudangPintar.Model;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace GudangPintar.Controllers
@@ -10,14 +11,37 @@ namespace GudangPintar.Controllers
 
         public List<Stock> GetAll() => stocks;
 
-        public void Add(Stock s)
+        public bool Add(Stock s)
         {
-            stocks.Add(s);
-        }
+            if (stocks.Any(x => x.NamaBarang == s.NamaBarang))
+            {
+                Console.WriteLine("Nama barang sudah ada!");
+                return false;
+            }
 
-        public void Delete(string nama)
+            if (s.Jumlah <= 0)
+            {
+                Console.WriteLine("Jumlah harus lebih dari 0!");
+                return false;
+            }
+
+            Debug.Assert(s.Jumlah > 0);
+
+            stocks.Add(s);
+            return true;
+        }
+        public bool Delete(string nama)
         {
-            stocks.RemoveAll(s => s.NamaBarang == nama);
+            var s = Get(nama);
+
+            if (s == null)
+            {
+                Console.WriteLine("Barang tidak ditemukan!");
+                return false;
+            }
+
+            stocks.Remove(s);
+            return true;
         }
 
         public Stock? Get(string nama)
@@ -28,20 +52,67 @@ namespace GudangPintar.Controllers
         public void Update(string nama, string newNama, Category kategori, double harga)
         {
             var s = Get(nama);
+
+            if (stocks.Any(x => x.NamaBarang == newNama && x.NamaBarang != nama))
+            {
+                Console.WriteLine("Nama barang sudah digunakan!");
+                return;
+            }
+
             if (s != null)
             {
                 s.EditStock(newNama, kategori, harga);
             }
         }
 
-        public void TambahStok(string nama, int jumlah)
+        public bool TambahStok(string nama, int jumlah)
         {
-            Get(nama)?.TambahStok(jumlah);
+            if (jumlah <= 0)
+            {
+                Console.WriteLine("Input harus berupa bilangan positif!");
+                return false;
+            }
+
+            var s = Get(nama);
+
+            if (s == null)
+            {
+                Console.WriteLine("Barang tidak ditemukan!");
+                return false;
+            }
+
+            Debug.Assert(jumlah > 0);
+
+            s.TambahStok(jumlah);
+            return true;
         }
 
-        public void KurangiStok(string nama, int jumlah)
+        public bool KurangiStok(string nama, int jumlah)
         {
-            Get(nama)?.KurangiStok(jumlah);
+            var s = Get(nama);
+
+            if (s == null)
+            {
+                Console.WriteLine("Barang tidak ditemukan!");
+                return false;
+            }
+
+            if (jumlah > s.Jumlah)
+            {
+                Console.WriteLine("Stok tidak cukup!");
+                return false;
+            }
+
+            if (jumlah <= 0)
+            {
+                Console.WriteLine("Jumlah harus lebih dari 0!");
+                return false;
+            }
+
+            Debug.Assert(s.Jumlah >= 0);
+
+            s.KurangiStok(jumlah);
+            return true;
         }
     }
 }
