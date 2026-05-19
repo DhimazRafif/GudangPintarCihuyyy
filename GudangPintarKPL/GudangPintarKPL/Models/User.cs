d6eb6eb9cfbf0d7953e7182e5ecad4e0da71df8a
banufasaadigana-hash <banufasaadigana@gmail.com>
2026-05-19 18:19:57 +0700

    menambahkan Table Driven yang hilang

--- GudangPintarKPL/GudangPintarKPL/Models/User.cs
+++ GudangPintarKPL/GudangPintarKPL/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace GudangPintarKPL.Models
@@ -12,6 +13,14 @@ namespace GudangPintarKPL.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public Role RoleUser { get; set; }
+
+        public User(int id, string username, string email, Role role)
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            RoleUser = role;
+        }
 
         public string[] getRowData() => new[] { Id.ToString(), Username, Email,RoleUser.ToString()};
     }
