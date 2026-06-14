2f331b8b9eff7ce431f322f3c0d3addc690a545f
DhimazRafif <hanafidhimaz@gmail.com>
2026-06-14 20:20:20 +0700

    fix: Menghilangkan button akun pada DashboardPegawai dan menambahkan kolom category di kedua dashboard

--- GudangPintarGui/Models/Barang.cs
+++ GudangPintarGui/Models/Barang.cs
@@ -8,6 +8,7 @@ namespace GudangPintarGui.Models
     {
         public int Id { get; set; }
         public string Nama { get; set; }
+        public string Category {  get; set; }
         public double Harga { get; set; }
         public int Jumlah { get; set; }
     }
