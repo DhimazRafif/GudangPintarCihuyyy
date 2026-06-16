52c6fc565f01ca01cf1ce800b30c47ea418a82b2
SushiDotRoll <irsyad.spramudito@gmail.com>
2026-06-16 15:34:34 +0700

    Menambahkan GUI history dan Notification

---
+++ GudangPintarGui/View/RiwayatPegawaiView.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace GudangPintarGui.View
+{
+    partial class RiwayatPegawaiView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnLogout = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel2 = new Panel();
+            lblTotalBarang = new Label();
+            label3 = new Label();
+            panel3 = new Panel();
+            lblTotalStok = new Label();
+            label2 = new Label();
+            dgvRiwayat = new DataGridView();
+            ClmID = new DataGridViewTextBoxColumn();
+            ClmBarang = new DataGridViewTextBoxColumn();
+            ClmQuantity = new DataGridViewTextBoxColumn();
+            ClmUser = new DataGridViewTextBoxColumn();
+            ClmSupplier = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRiwayat).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LawnGreen;
+            panel1.Controls.Add(btnLogout);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(3, 5);
+            panel1.Margin = new Padding(4, 3, 4, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(340, 1258);
+            panel1.TabIndex = 1;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.DarkGray;
+            btnLogout.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = SystemColors.ControlText;
+            btnLogout.Location = new Point(73, 1073);
+            btnLogout.Margin = new Padding(4, 5, 4, 5);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(174, 52);
+            btnLogout.TabIndex = 11;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(73, 482);
+            button3.Margin = new Padding(4, 5, 4, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(174, 62);
+            button3.TabIndex = 8;
+            button3.Text = "Riwayat";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(73, 372);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(174, 65);
+            button2.TabIndex = 7;
+            button2.Text = "Barang";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(73, 265);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 63);
+            button1.TabIndex = 6;
+            button1.Text = "Dashboard";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.LawnGreen;
+            panel2.Controls.Add(lblTotalBarang);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(351, 5);
+            panel2.Margin = new Padding(4, 5, 4, 5);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(700, 187);
+            panel2.TabIndex = 21;
+            // 
+            // lblTotalBarang
+            // 
+            lblTotalBarang.AutoSize = true;
+            lblTotalBarang.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
+            lblTotalBarang.ForeColor = Color.White;
+            lblTotalBarang.Location = new Point(291, 63);
+            lblTotalBarang.Margin = new Padding(4, 0, 4, 0);
+            lblTotalBarang.Name = "lblTotalBarang";
+            lblTotalBarang.Size = new Size(102, 45);
+            lblTotalBarang.TabIndex = 3;
+            lblTotalBarang.Text = "label1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(260, 22);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(181, 40);
+            label3.TabIndex = 1;
+            label3.Text = "Total Barang";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.LawnGreen;
+            panel3.Controls.Add(lblTotalStok);
+            panel3.Controls.Add(label2);
+            panel3.Location = new Point(1060, 5);
+            panel3.Margin = new Padding(4, 5, 4, 5);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(744, 187);
+            panel3.TabIndex = 22;
+            // 
+            // lblTotalStok
+            // 
+            lblTotalStok.AutoSize = true;
+            lblTotalStok.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
+            lblTotalStok.ForeColor = Color.White;
+            lblTotalStok.Location = new Point(276, 63);
+            lblTotalStok.Margin = new Padding(4, 0, 4, 0);
+            lblTotalStok.Name = "lblTotalStok";
+            lblTotalStok.Size = new Size(107, 45);
+            lblTotalStok.TabIndex = 2;
+            lblTotalStok.Text = "label4";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(259, 22);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(145, 40);
+            label2.TabIndex = 0;
+            label2.Text = "Total Stok";
+            // 
+            // dgvRiwayat
+            // 
+            dgvRiwayat.AllowUserToAddRows = false;
+            dgvRiwayat.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvRiwayat.BackgroundColor = Color.DarkGray;
+            dgvRiwayat.BorderStyle = BorderStyle.None;
+            dgvRiwayat.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgvRiwayat.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dgvRiwayat.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRiwayat.Columns.AddRange(new DataGridViewColumn[] { ClmID, ClmBarang, ClmQuantity, ClmUser, ClmSupplier });
+            dgvRiwayat.Location = new Point(351, 270);
+            dgvRiwayat.Margin = new Padding(4, 5, 4, 5);
+            dgvRiwayat.MultiSelect = false;
+            dgvRiwayat.Name = "dgvRiwayat";
+            dgvRiwayat.RowHeadersVisible = false;
+            dgvRiwayat.RowHeadersWidth = 62;
+            dgvRiwayat.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvRiwayat.Size = new Size(1453, 995);
+            dgvRiwayat.TabIndex = 23;
+            // 
+            // ClmID
+            // 
+            ClmID.HeaderText = "ID";
+            ClmID.MinimumWidth = 8;
+            ClmID.Name = "ClmID";
+            // 
+            // ClmBarang
+            // 
+            ClmBarang.HeaderText = "Nama Barang";
+            ClmBarang.MinimumWidth = 8;
+            ClmBarang.Name = "ClmBarang";
+            // 
+            // ClmQuantity
+            // 
+            ClmQuantity.HeaderText = "Banyak Stok";
+            ClmQuantity.MinimumWidth = 8;
+            ClmQuantity.Name = "ClmQuantity";
+            // 
+            // ClmUser
+            // 
+            ClmUser.HeaderText = "Diganti Oleh";
+            ClmUser.MinimumWidth = 8;
+            ClmUser.Name = "ClmUser";
+            // 
+            // ClmSupplier
+            // 
+            ClmSupplier.HeaderText = "Dari Supplier";
+            ClmSupplier.MinimumWidth = 8;
+            ClmSupplier.Name = "ClmSupplier";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(351, 197);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 60);
+            label1.TabIndex = 24;
+            label1.Text = "Riwayat";
+            // 
+            // RiwayatPegawaiView
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1806, 1268);
+            Controls.Add(label1);
+            Controls.Add(dgvRiwayat);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "RiwayatPegawaiView";
+            Text = "RiwayatPegawaiView";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvRiwayat).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnLogout;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Panel panel2;
+        private Label lblTotalBarang;
+        private Label label3;
+        private Panel panel3;
+        private Label lblTotalStok;
+        private Label label2;
+        private DataGridView dgvRiwayat;
+        private DataGridViewTextBoxColumn ClmID;
+        private DataGridViewTextBoxColumn ClmBarang;
+        private DataGridViewTextBoxColumn ClmQuantity;
+        private DataGridViewTextBoxColumn ClmUser;
+        private DataGridViewTextBoxColumn ClmSupplier;
+        private Label label1;
+    }
+}
