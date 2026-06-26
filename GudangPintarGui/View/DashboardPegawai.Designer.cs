8239c00a3e4790abca890f183bb5126298caf016
Mikamikkk <reugan0675@gmail.com>
2026-06-26 17:54:12 +0700

    memperbaiki GUI dan Memperbaiki clean code

--- GudangPintarGui/View/DashboardPegawai.Designer.cs
+++ GudangPintarGui/View/DashboardPegawai.Designer.cs
@@ -51,10 +51,10 @@
             // panel4
             // 
             panel4.Controls.Add(dgvBarangPegawai);
-            panel4.Location = new Point(358, 196);
-            panel4.Margin = new Padding(4);
+            panel4.Location = new Point(251, 118);
+            panel4.Margin = new Padding(3, 2, 3, 2);
             panel4.Name = "panel4";
-            panel4.Size = new Size(1448, 1066);
+            panel4.Size = new Size(1014, 640);
             panel4.TabIndex = 4;
             // 
             // dgvBarangPegawai
@@ -62,21 +62,19 @@
             dgvBarangPegawai.BackgroundColor = SystemColors.ControlLightLight;
             dgvBarangPegawai.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvBarangPegawai.Location = new Point(0, 0);
-            dgvBarangPegawai.Margin = new Padding(4, 5, 4, 5);
             dgvBarangPegawai.Name = "dgvBarangPegawai";
             dgvBarangPegawai.RowHeadersWidth = 51;
-            dgvBarangPegawai.Size = new Size(1448, 1066);
+            dgvBarangPegawai.Size = new Size(1014, 640);
             dgvBarangPegawai.TabIndex = 0;
             // 
             // panel3
             // 
-            panel3.BackColor = Color.LawnGreen;
+            panel3.BackColor = Color.LimeGreen;
             panel3.Controls.Add(lblTotalStokPegawai);
             panel3.Controls.Add(label1);
-            panel3.Location = new Point(1060, 5);
-            panel3.Margin = new Padding(4, 5, 4, 5);
+            panel3.Location = new Point(742, 3);
             panel3.Name = "panel3";
-            panel3.Size = new Size(744, 186);
+            panel3.Size = new Size(521, 112);
             panel3.TabIndex = 20;
             // 
             // lblTotalStokPegawai
@@ -84,10 +82,9 @@
             lblTotalStokPegawai.AutoSize = true;
             lblTotalStokPegawai.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
             lblTotalStokPegawai.ForeColor = Color.White;
-            lblTotalStokPegawai.Location = new Point(276, 64);
-            lblTotalStokPegawai.Margin = new Padding(4, 0, 4, 0);
+            lblTotalStokPegawai.Location = new Point(193, 38);
             lblTotalStokPegawai.Name = "lblTotalStokPegawai";
-            lblTotalStokPegawai.Size = new Size(107, 45);
+            lblTotalStokPegawai.Size = new Size(74, 30);
             lblTotalStokPegawai.TabIndex = 2;
             lblTotalStokPegawai.Text = "label4";
             // 
@@ -96,22 +93,20 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.Location = new Point(259, 21);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(181, 13);
             label1.Name = "label1";
-            label1.Size = new Size(145, 40);
+            label1.Size = new Size(95, 25);
             label1.TabIndex = 0;
             label1.Text = "Total Stok";
             // 
             // panel2
             // 
-            panel2.BackColor = Color.LawnGreen;
+            panel2.BackColor = Color.LimeGreen;
             panel2.Controls.Add(lblTotalBarangPegawai);
             panel2.Controls.Add(label2);
-            panel2.Location = new Point(351, 5);
-            panel2.Margin = new Padding(4, 5, 4, 5);
+            panel2.Location = new Point(246, 3);
             panel2.Name = "panel2";
-            panel2.Size = new Size(700, 186);
+            panel2.Size = new Size(490, 112);
             panel2.TabIndex = 19;
             // 
             // lblTotalBarangPegawai
@@ -119,10 +114,9 @@
             lblTotalBarangPegawai.AutoSize = true;
             lblTotalBarangPegawai.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
             lblTotalBarangPegawai.ForeColor = Color.White;
-            lblTotalBarangPegawai.Location = new Point(292, 69);
-            lblTotalBarangPegawai.Margin = new Padding(4, 0, 4, 0);
+            lblTotalBarangPegawai.Location = new Point(204, 41);
             lblTotalBarangPegawai.Name = "lblTotalBarangPegawai";
-            lblTotalBarangPegawai.Size = new Size(102, 45);
+            lblTotalBarangPegawai.Size = new Size(70, 30);
             lblTotalBarangPegawai.TabIndex = 2;
             lblTotalBarangPegawai.Text = "label1";
             // 
@@ -131,24 +125,23 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ActiveCaptionText;
-            label2.Location = new Point(260, 21);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(182, 13);
             label2.Name = "label2";
-            label2.Size = new Size(181, 40);
+            label2.Size = new Size(118, 25);
             label2.TabIndex = 1;
             label2.Text = "Total Barang";
             // 
             // panel1
             // 
-            panel1.BackColor = Color.LawnGreen;
+            panel1.BackColor = Color.LimeGreen;
             panel1.Controls.Add(btnLogout);
             panel1.Controls.Add(button3);
             panel1.Controls.Add(button2);
             panel1.Controls.Add(button1);
-            panel1.Location = new Point(2, 5);
-            panel1.Margin = new Padding(4);
+            panel1.Location = new Point(1, 3);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(340, 1259);
+            panel1.Size = new Size(238, 755);
             panel1.TabIndex = 18;
             // 
             // btnLogout
@@ -156,10 +149,9 @@
             btnLogout.BackColor = Color.DarkGray;
             btnLogout.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnLogout.ForeColor = SystemColors.ControlText;
-            btnLogout.Location = new Point(72, 1074);
-            btnLogout.Margin = new Padding(4, 5, 4, 5);
+            btnLogout.Location = new Point(50, 644);
             btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(174, 51);
+            btnLogout.Size = new Size(122, 31);
             btnLogout.TabIndex = 8;
             btnLogout.Text = "Logout";
             btnLogout.UseVisualStyleBackColor = false;
@@ -168,10 +160,9 @@
             // button3
             // 
             button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.Location = new Point(72, 481);
-            button3.Margin = new Padding(4, 5, 4, 5);
+            button3.Location = new Point(50, 289);
             button3.Name = "button3";
-            button3.Size = new Size(174, 61);
+            button3.Size = new Size(122, 37);
             button3.TabIndex = 7;
             button3.Text = "Riwayat";
             button3.UseVisualStyleBackColor = true;
@@ -180,10 +171,9 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(72, 371);
-            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Location = new Point(50, 223);
             button2.Name = "button2";
-            button2.Size = new Size(174, 65);
+            button2.Size = new Size(122, 39);
             button2.TabIndex = 6;
             button2.Text = "Barang";
             button2.UseVisualStyleBackColor = true;
@@ -192,26 +182,25 @@
             // button1
             // 
             button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(72, 265);
-            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Location = new Point(50, 159);
             button1.Name = "button1";
-            button1.Size = new Size(174, 64);
+            button1.Size = new Size(122, 38);
             button1.TabIndex = 5;
             button1.Text = "Dashboard";
             button1.UseVisualStyleBackColor = true;
             // 
             // DashboardPegawai
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             BackColor = Color.White;
-            ClientSize = new Size(1806, 1269);
+            ClientSize = new Size(1264, 637);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(panel4);
-            Margin = new Padding(4);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "DashboardPegawai";
             Text = "Form1";
             Load += DashboardPegawai_Load;
